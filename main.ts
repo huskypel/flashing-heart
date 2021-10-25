cdaba51ba47a69cdf6e0b8531b2ac933ff8a8766
huskypel <92863057+huskypel@users.noreply.github.com>
2021-10-25 11:18:34 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . # . .
+        . . . . .
+        `)
+})
